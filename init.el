418f0c88c2dc8f1b5d81d256b3d69803b05e38ac
gotomon365 <gotomon365@gmail.com>
2020-06-21 10:25:24 +0800

    Add files via upload

---
+++ init.el
@@ -0,0 +1,34 @@
+;;;
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+         '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (auto-complete avy iedit expand-region expand-region-abbrevs aggresive-indent hungry-delete beacon yasnippet-snippets elpy yasnippet flycheck zeno-theme zenburn-theme which-key use-package try orgalist org-bullets org-ac org multiple-cursors diminish counsel color-theme-modern color-theme ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
