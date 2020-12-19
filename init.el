b9a75cb567505c7bff648682191accc62f6b1024
Kuei <gotomon365@gmail.com>
2020-12-19 21:00:35 +0800

    add new file

--- init.el
+++ init.el
@@ -1,10 +1,30 @@
 ;;;
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s) (end-of-buffer) (eval-print-last-sexp))))
+
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives
          '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("marmalade" . "http:// marmalade-repo.org/packages/")
+	("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s) (end-of-buffer) (eval-print-last-sexp))))
 
 (package-initialize)
 
