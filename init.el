fbf8fa3b9d3f77afb9c4624da31f7c7a001b6891
MKJH <mkjh@MKJHs-MacBook-Pro.local>
2021-01-01 15:57:02 +0800

    reset my init.el and myinit.org

--- init.el
+++ init.el
@@ -1,51 +1,22 @@
-;;;
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
-   (lambda (s) (end-of-buffer) (eval-print-last-sexp))))
-
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives
-         '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(package-initialize)
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http:// marmalade-repo.org/packages/")
-	("melpa" . "http://melpa.milkbox.net/packages/")))
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
-   (lambda (s) (end-of-buffer) (eval-print-last-sexp))))
-
+'("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Bootstrap 'use-package'
+;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-
+(package-refresh-contents)
+(package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (auto-complete avy iedit expand-region expand-region-abbrevs aggresive-indent hungry-delete beacon yasnippet-snippets elpy yasnippet flycheck zeno-theme zenburn-theme which-key use-package try orgalist org-bullets org-ac org multiple-cursors diminish counsel color-theme-modern color-theme ace-window))))
+   '(zenburn-theme expand-region hungry-delete beacon elpy yasnippet flycheck swiper ivy ace-window which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
