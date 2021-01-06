3a216049db63e057cd6b8a43cb41c738fbb09a26
MKJH <mkjh@MKJHs-MacBook-Pro.local>
2021-01-06 22:25:37 +0800

    modified init.el

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme expand-region hungry-delete beacon elpy yasnippet flycheck swiper ivy ace-window which-key try use-package)))
+   '(yasnippet-snippets zenburn-theme expand-region hungry-delete beacon elpy yasnippet flycheck swiper ivy ace-window which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
