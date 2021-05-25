d049eaee9c4fb5098a632b17aaf9004abd5097ee
gotomon <gotomon365@gmail.com>
2021-05-25 08:40:48 +0800

    modified init.el myinit.org

--- init.el
+++ init.el
@@ -1,15 +1,13 @@
-;;;;;;;kuei
 (require 'package)
-
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https:/elpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/"))
+
 (package-initialize)
-;;
-(setq gnutls-algorithm-priority "NORMAL: -VERS-TLS1.3")
-;;
+
+;; Bootstrap `user-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-;;;;
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
